8a8ec046b26430d3bd80785f9e1bc75c04bbedf5
Anukool <ascmailing0@gmail.com>
2025-03-16 13:00:21 +0530

    default parameters and keyword arguments

--- 02_functions/functions.py
+++ 02_functions/functions.py
@@ -1,6 +1,31 @@
-name = input("Enter your name: ")
+#########################
+# name = input("Enter your name: ")
 
-def greet(name):
-    print(f"Hello, {name}!")
+# def greet(name):
+#     print(f"Hello, {name}!")
 
-greet(name)
+# greet(name)
+
+#########################
+# Return Values
+# def add(a, b):
+#     return a + b
+
+# result = add(3, 5)
+# print(result)
+
+#########################
+# # default parameters
+# def greet(name="user"):
+#     print(f"Hello, {name}")
+
+# greet()
+# greet("Anukool")
+
+# Keyword arguments
+# def introduce(name, age):
+#     print(f"My name is {name}, and I am {age} years old")
+# introduce(age=25, name="Anukool")
+
+############################
+# Variable length arguments
