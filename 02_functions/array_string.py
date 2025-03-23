fa9bf38a9272321c097a5f69f6855f4c8bf01599
Anukool <ascmailing0@gmail.com>
2025-03-23 15:34:19 +0530

    array string

---
+++ 02_functions/array_string.py
@@ -0,0 +1,13 @@
+def mutate_string(string, position, character):
+    a = list(string)
+    a.insert(position, character)
+    return "".join(a)
+
+print(mutate_string("abracadabra", 5, "k"))
+
+
+# a = [1,2,3,4,5,6]
+
+# a(2) = "55"
+
+# print(a)
