66eb2ddd1a4542a886d04d77fb3127d79d6aa78d
Anukool <ascmailing0@gmail.com>
2025-04-05 15:00:57 +0530

    simple concatenation operation on string

---
+++ 01_basics/string.py
@@ -0,0 +1,8 @@
+s1 = 'hello' # single quote 
+s2 = "Python" # double quote still the same string data type
+s3 = '''multi line 
+string in python''' # multi line strings
+
+concate_str = "Hello" + " " + "Python" # Basic concatenation
+
+print(concate_str)
