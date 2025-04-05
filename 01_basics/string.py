1a0306dec971565c59b92db9fd4aeb8994aac93d
Anukool <ascmailing0@gmail.com>
2025-04-05 15:15:09 +0530

    slicing over string

--- 01_basics/string.py
+++ 01_basics/string.py
@@ -6,3 +6,17 @@ string in python''' # multi line strings
 concate_str = "Hello" + " " + "Python" # Basic concatenation
 
 print(concate_str)
+
+#repetition
+print("python " * 4) # 'python python python python'
+
+#indexing
+s = 'PythonIsGood'
+s[0] # 'P'
+s[-1] # "g"
+
+#Slicing
+print(s[2:10]) # 'thonIsGo'
+print(s[2:]) # 'py'is sliced so 'thonIsGood' is result
+print(s[:10]) # 'od' is sliced so 'PythonIsGood' is result
+
